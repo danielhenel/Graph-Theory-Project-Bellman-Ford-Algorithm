34c7e20c76c6d6caf12d858442e63ef1710d2764
Daniel <danielhenel@student.agh.edu.pl>
2021-05-29 22:34:11 +0200

    improving code quality

--- bellman_ford/src/bellman_ford.cpp
+++ bellman_ford/src/bellman_ford.cpp
@@ -31,3 +31,33 @@ void Paths::printPathToNodeWithGivenIndex(int index){
         cout<<endl<<"The algorithm didn't find the shortest path, because the graph has negative cycles!"<<endl;
     }
 }
+
+void Paths::relaxation(Graph::Edge * edge){
+    double w = edge->getValue();
+    int v = edge->getEndNodeIndex();
+    int u = edge->getStartNodeIndex();
+    if(allPathNodes[v].minValue > allPathNodes[u].minValue + w){
+        allPathNodes[v].minValue = allPathNodes[u].minValue + w;
+        allPathNodes[v].prevNode = graph->getNode(u);
+    }
+}
+
+bool Paths::check(Graph::Edge * edge){
+    double w = edge->getValue();
+    int v = edge->getEndNodeIndex();
+    int u = edge->getStartNodeIndex();
+    if(allPathNodes[v].minValue > allPathNodes[u].minValue + w)
+        return false;
+    return true;
+}
+
+bool Paths::bellmanFord(){
+    allPathNodes[startNodeIndex] = {0, nullptr};
+    for(int i = 0; i < N - 1; i++)
+        for(int j = 0; j < E; j++)
+            relaxation(graph->getEdge(j));
+
+    for(int j = 0; j < E; j++)
+        if(!(check(graph->getEdge(j)))) return false;
+    return true;
+}
--- bellman_ford/src/bellman_ford.h
+++ bellman_ford/src/bellman_ford.h
@@ -15,40 +15,14 @@ class Paths{
         int E;
         int GraphWithoutNegativeCycles;
         PathNode * allPathNodes;
-        void relaxation(Graph::Edge * edge){
-            double w = edge->getValue();
-            int v = edge->getEndNodeIndex();
-            int u = edge->getStartNodeIndex();
-            if(allPathNodes[v].minValue > allPathNodes[u].minValue + w){
-                allPathNodes[v].minValue = allPathNodes[u].minValue + w;
-                allPathNodes[v].prevNode = graph->getNode(u);
-            }
-        }
-        bool check(Graph::Edge * edge){
-            double w = edge->getValue();
-            int v = edge->getEndNodeIndex();
-            int u = edge->getStartNodeIndex();
-            if(allPathNodes[v].minValue > allPathNodes[u].minValue + w)
-                return false;
-            return true;
-        }
-        bool bellmanFord(){
-            allPathNodes[startNodeIndex] = {0, nullptr};
-            for(int i = 0; i < N - 1; i++)
-                for(int j = 0; j < E; j++)
-                    relaxation(graph->getEdge(j));
-
-            for(int j = 0; j < E; j++)
-                if(!(check(graph->getEdge(j)))) return false;
-            return true;
-        }
+        void relaxation(Graph::Edge * edge);
+        bool check(Graph::Edge * edge);
+        bool bellmanFord();
         void printPathAndCalculateTheLength(int index, double &length);
 
     public:
         Paths(Graph * graph, int startNodeIndex);
         void printPathToNodeWithGivenIndex(int index);
-
-
 };
 
 
